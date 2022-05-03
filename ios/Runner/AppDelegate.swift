8f0b86d453ee840164d807fcb87a09fea1cb3732
Dan Potter <dpotter@mitre.org>
2022-05-03 19:08:44 -0400

    Actually request access to HealthKit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,22 +4,59 @@ import HealthKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-      let healthKitChannel = FlutterMethodChannel(name: "mitre.org/rosie/healthkit", binaryMessenger: controller.binaryMessenger)
-      healthKitChannel.setMethodCallHandler({
-            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-          if (call.method == "isHealthDataAvailable") {
-              result(HKHealthStore.isHealthDataAvailable())
-          } else {
-              result(FlutterMethodNotImplemented)
-          }
-      })
+    lazy var healthStore = HKHealthStore()
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let healthKitChannel = FlutterMethodChannel(name: "mitre.org/rosie/healthkit", binaryMessenger: controller.binaryMessenger)
+        healthKitChannel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            switch (call.method) {
+            case "isHealthDataAvailable":
+                result(HKHealthStore.isHealthDataAvailable())
+            case "requestAccess":
+                self?.requestHealthKitAccess(result: result)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        })
+
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    func requestHealthKitAccess(result: @escaping FlutterResult) {
+        if #available(iOS 12.0, *) {
+            // Create the sample types if possible
+            var types = Set<HKObjectType>()
+            for type in [
+                HKClinicalTypeIdentifier.allergyRecord,
+                HKClinicalTypeIdentifier.conditionRecord,
+                HKClinicalTypeIdentifier.immunizationRecord,
+                HKClinicalTypeIdentifier.labResultRecord,
+                HKClinicalTypeIdentifier.medicationRecord,
+                HKClinicalTypeIdentifier.procedureRecord,
+                HKClinicalTypeIdentifier.vitalSignRecord
+            ] {
+                if let clinicalType = HKObjectType.clinicalType(forIdentifier: type) {
+                    types.insert(clinicalType)
+                }
+            }
+            healthStore.requestAuthorization(toShare: nil, read: types, completion: { success, error in
+                // The result happens in a background thread, but we want to invoke Flutter only from the main thread, so:
+                DispatchQueue.main.async {
+                    if let error = error {
+                        result(FlutterError(code: "HealthKitError", message: error.localizedDescription, details: error))
+                    } else {
+                        result(success)
+                    }
+                }
+            })
+        } else {
+            result(FlutterError(code: "HealthKitUnavailable", message: "HealthKit not available", details: nil))
+        }
+    }
 }
--- lib/src/data_source/health_kit/health_kit.dart
+++ lib/src/data_source/health_kit/health_kit.dart
@@ -2,17 +2,16 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 
-// HealthKit class. This represents the HKHealthStore class in Objective-C.
-// Note that while you can construct this, it requires
+/// HealthKit interface: provides methods for accessing HealthKit data.
 class HealthKit {
   // Channel through which HealthKit requests are sent
   static const platform = MethodChannel('mitre.org/rosie/healthkit');
 
   HealthKit();
 
-  // Check if HealthKit is supported on this device. Can only return true on
-  // iOS devices, and specifically iPhones. If this method returns false, then
-  // all other function calls will return null!
+  /// Check if HealthKit is supported on this device. Can only return true on
+  /// iOS devices, and specifically iPhones. If this method returns false, then
+  /// all other function calls will return null!
   static Future<bool> isHealthDataAvailable() async {
     // Only possibly available on iOS
     if (Platform.isIOS) {
@@ -30,5 +29,12 @@ class HealthKit {
     } else {
       return false;
     }
+  }
+
+  /// Attempts to request access to HealthKit data. The exact fields that access is requested to are defined in the iOS
+  /// portion of the app, this simply tells the iOS code to invoke them. The returned bool indicates whether this
+  /// operation was successful.
+  static Future<bool> requestAccess() async {
+    return await platform.invokeMethod('requestAccess');
   }
 }
