22c4bf94cd106152fce741741f5ba25a8b544d00
Dr. Beth A Yost <BETHYOST@MITRE.ORG>
2022-08-16 14:30:38 -0400

    remove placeholder box from home page

--- lib/src/data_source/list_data_sources_screen.dart
+++ lib/src/data_source/list_data_sources_screen.dart
@@ -16,43 +16,38 @@ import 'package:flutter/material.dart';
 import 'data_source.dart';
 
 class _DataSourceDescription extends StatelessWidget {
-  const _DataSourceDescription({Key? key, required this.dataSource}) : super(key: key);
+  const _DataSourceDescription({Key? key, required this.dataSource})
+      : super(key: key);
 
   final DataSource dataSource;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(dataSource.name,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.titleLarge
-        ),
-        const SizedBox(height: 2),
-        Text(dataSource.description,
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.caption
-        ),
-        Expanded(child: Container()),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              // When connecting to a data source, force it into a scaffold
-              MaterialPageRoute(
-                builder: (context) => Scaffold(
-                  appBar: AppBar(title: Text("Connect to ${dataSource.name}")),
-                  body: dataSource.createConnectionScreen(context)
-                )
-              )
-            );
-          },
-          child: const Text("Connect")
-        )
-      ]
-    );
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(dataSource.name,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 2),
+          Text(dataSource.description,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.caption),
+          Expanded(child: Container()),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    // When connecting to a data source, force it into a scaffold
+                    MaterialPageRoute(
+                        builder: (context) => Scaffold(
+                            appBar: AppBar(
+                                title: Text("Connect to ${dataSource.name}")),
+                            body: dataSource.createConnectionScreen(context))));
+              },
+              child: const Text("Connect"))
+        ]);
   }
 }
 
@@ -63,30 +58,27 @@ class _DataSourceTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
-      child: SizedBox(
-        height: 120,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Force the icon into a 72x72 box
-            SizedBox(
-              width: 72.0,
-              child: AspectRatio(
-                aspectRatio: 1.0,
-                child: dataSource.createIcon(context) ?? const Placeholder()
-              )
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 2, 0),
-                child: _DataSourceDescription(dataSource: dataSource)
-              )
-            )
-          ]
-        )
-      )
-    );
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+        child: SizedBox(
+            height: 120,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // Force the icon into a 72x72 box
+                  SizedBox(
+                      width: 72.0,
+                      child: AspectRatio(
+                          aspectRatio: 1.0,
+                          child: dataSource
+                              .createIcon(context) // ?? const Placeholder()
+                          )),
+                  Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 2, 0),
+                          child:
+                              _DataSourceDescription(dataSource: dataSource)))
+                ])));
   }
 }
 
@@ -114,35 +106,36 @@ class _ListDataSourcesState extends State<ListDataSourcesScreen> {
         }
       });
     }, onDone: () {
-      setState(() { _lookingUp = false; });
+      setState(() {
+        _lookingUp = false;
+      });
     });
   }
 
   Widget _buildDataSourceList(BuildContext context) {
     if (_lookingUp) {
-      return Center(child:
-          Column(children: const [
-            CircularProgressIndicator(),
-            Text("Finding available data sources...")
-          ])
-        );
+      return Center(
+          child: Column(children: const [
+        CircularProgressIndicator(),
+        Text("Finding available data sources...")
+      ]));
     } else {
       // Otherwise, build the list!
       if (_dataSources.isEmpty) {
         return const Center(child: Text("No data sources."));
       } else {
         return ListView.builder(
-          itemBuilder: (context, index) => _DataSourceTile(dataSource: _dataSources[index]),
-          itemCount: _dataSources.length
-        );
+            itemBuilder: (context, index) =>
+                _DataSourceTile(dataSource: _dataSources[index]),
+            itemCount: _dataSources.length);
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Connect Data & Services")),
-      body: _buildDataSourceList(context)
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text("Connect Data & Services")),
+        body: _buildDataSourceList(context));
   }
 }
