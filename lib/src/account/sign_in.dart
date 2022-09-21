1a07a486cfa8455ab0fde48c6ba850f1efcebd35
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-09-21 16:50:03 -0400

    Fix sign in page so that it also ensures email and password are not the empty string

--- lib/src/account/sign_in.dart
+++ lib/src/account/sign_in.dart
@@ -110,14 +110,19 @@ class _SignInState extends State<SignIn> {
       submitLabel: localizations.signInButton,
       onSubmit: () async {
         if (email != null && password != null) {
-          if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email!)) {
+          if (email == '' && password == '') {
+            return "Email and password are required";
+          }
+           else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email!)) {
             return "The email entered is not a valid email address. Please try again.";
           }
-          
           final auth =
               await context.read<OpenHealthManager>().signIn(email!, password!);
-          return auth == null ? localizations.loginFailed : null;
-        } else {
+          return auth == null
+              ? localizations.loginFailed
+              : null;
+        } 
+        else {
           return localizations.emailPasswordRequired;
         }
       },
