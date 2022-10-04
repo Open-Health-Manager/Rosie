0320a627cafb1fa2e71a41bfcaf48479a9e4aae2
Dan Potter <dpotter@mitre.org>
2022-10-04 15:24:44 -0400

    Add debug screen for HealthKit

--- lib/src/debug_details.dart
+++ lib/src/debug_details.dart
@@ -15,6 +15,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'open_health_manager/open_health_manager.dart';
+import 'data_source/health_kit/health_kit.dart';
+import 'data_source/health_kit/health_kit_debug.dart';
 
 /// Shows debug details in a Scaffold.
 class DebugDetailsScreen extends StatelessWidget {
@@ -30,8 +32,26 @@ class DebugDetailsScreen extends StatelessWidget {
   }
 }
 
-class DebugDetails extends StatelessWidget {
+/// Provides more detailed information about the current environment
+class DebugDetails extends StatefulWidget {
   const DebugDetails({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _DebugDetailsState();
+}
+
+class _DebugDetailsState extends State<DebugDetails> {
+  var _healthKitAvailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    HealthKit.isHealthDataAvailable().then((available) {
+      setState(() {
+        _healthKitAvailable = available;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +62,22 @@ class DebugDetails extends StatelessWidget {
           title: const Text('OHM Endpoint'),
           subtitle: Text(healthManager.serverUrl.toString()),
         ),
+        if (_healthKitAvailable)
+          ListTile(
+            title: const Text('HealthKit'),
+            subtitle: const Text(
+                'Showing details will trigger a HealthKit permission request'),
+            trailing: TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HealthKitDebugScreen(),
+                    ));
+              },
+              child: const Text('Details'),
+            ),
+          )
       ],
     );
   }
